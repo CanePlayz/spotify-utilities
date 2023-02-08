a8975077b4bfbdb0af5c4d127b5473000c88caa9
CanePlayz <jjpfst@gmail.com>
2023-02-08 23:23:59 +0100

    All albums of an artist will be fetched now

--- classes/artist/methods/fetch_albums.py
+++ classes/artist/methods/fetch_albums.py
@@ -5,15 +5,33 @@ from tokens import access_token
 
 
 def main(artist_id):
-    query = "https://api.spotify.com/v1/artists/{}/albums".format(artist_id)
-    response = requests.get(query,
-                            headers={"Content-Type": "application/json",
-                                     "Authorization": "Bearer {}".format(access_token),
-                                     "limit": "50"})
-    match response.status_code:
-        case 200: pass
-        case _: raise APIError(response.status_code)
+
+    # Create variables
     albums = []
-    for album in response.json()["items"]:
-        albums.append(album["id"])
+    limit_reached = False
+    counter = 0
+
+    # Fetch albums until there are no more albums to be fetched
+    while limit_reached == False:
+
+        # Send a request to the Spotify API
+        query = f"https://api.spotify.com/v1/artists/{artist_id}/albums?offset={counter}&limit=50"
+        response = requests.get(query,
+                                headers={"Content-Type": "application/json",
+                                         "Authorization": f"Bearer {access_token}"})
+        # Check if the request was successful
+        match response.status_code:
+            case 200: pass
+            case _: raise APIError(response.status_code)
+
+        # Add the albums to the list
+        for album in response.json()["items"]:
+            albums.append(album["id"])
+
+        # Check if there are more albums to fetch
+        if response.json()["next"] == None:
+            limit_reached = True
+        else:
+            counter += 50
+
     return (albums)
--- classes/artist/methods/fetch_tracks.py
+++ classes/artist/methods/fetch_tracks.py
@@ -5,14 +5,25 @@ from tokens import access_token
 
 
 def main(albums):
+
+    # Create variables
     tracks = []
+
+    # Fetch tracks for each album
     for album in albums:
+
+        # Send a request to the Spotify API
         query = f"https://api.spotify.com/v1/albums/{album}/tracks"
         response = requests.get(query,
                                 headers={"Authorization": f"Bearer {access_token}"})
+
+        # Check if the request was successful
         match response.status_code:
             case 200: pass
             case _: raise APIError(response.status_code)
+
+        # Add the tracks to the list
         for track in response.json()["items"]:
             tracks.append(track["id"])
+
     return (tracks)
