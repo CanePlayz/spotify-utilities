17f38ad935e0dbb41ead557c85c65f447e12f657
CanePlayz <jjpfst@gmail.com>
2023-02-08 22:36:04 +0100

    Added functionality to fetch tracks

--- classes/artist/methods/fetch_tracks.py
+++ classes/artist/methods/fetch_tracks.py
@@ -1,7 +1,18 @@
-import json
 import requests
+
+from api.exceptions import APIError
 from tokens import access_token
 
 
-def main(album_id):
-    return ()
+def main(albums):
+    tracks = []
+    for album in albums:
+        query = f"https://api.spotify.com/v1/albums/{album}/tracks"
+        response = requests.get(query,
+                                headers={"Authorization": f"Bearer {access_token}"})
+        match response.status_code:
+            case 200: pass
+            case _: raise APIError(response.status_code)
+        for track in response.json()["items"]:
+            tracks.append(track["id"])
+    return (tracks)
