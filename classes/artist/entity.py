cc89a29efc3bc125439767f5f5e97a717ea53c73
CanePlayz <jjpfst@gmail.com>
2023-02-08 22:34:06 +0100

    Refactor & better status messages

--- classes/artist/entity.py
+++ classes/artist/entity.py
@@ -4,9 +4,9 @@ import api.exceptions as e
 
 class Artist(object):
 
-    def __init__(self, name, artistID):
+    def __init__(self, name, id):
         self.name = name
-        self.artistID = artistID
+        self.artistID = id
         self.albums = []
         self.tracks = []
         self.genres = []
@@ -16,38 +16,41 @@ class Artist(object):
         # self.fetch_albums()
         # self.fetch_tracks()
 
-    def fetch_artist(self, artistID):
+    def fetch_artist(self):
         print(f"Fetching information of {self.name}...")
         try:
-            m.fetch_artist.main(artistID)
+            res = m.fetch_artist.main(self.artistID)
         except e.APIError as err:
             print(
                 "Error while fetching information of {}}...: {} ({})".format(self.name,
                                                                              err.code, e.code_to_str_dict[err.code]))
         else:
-            self.artist = m.fetch_artist.main(artistID)
+            self.followers = res
+            # Set attributes
 
     def fetch_albums(self):
         print(f"Fetching albums of {self.name}...")
         try:
-            m.fetch_albums.main(self.artistID)
+            self.albums = m.fetch_albums.main(self.artistID)
         except e.APIError as err:
             print("Error while fetching albums of {}: {} ({})".format(self.name,
                                                                       err.code, e.code_to_str_dict[err.code]))
         else:
-            self.albums = m.fetch_albums.main(self.artistID)
-            print("Albums fetched successfully.")
+            print(
+                f"Successfully fetched {len(self.albums)} albums of {self.name}.")
+            print("Album IDs: " + str(self.albums))
 
     def fetch_tracks(self):
         print(f"Fetching tracks of {self.name}...")
         try:
-            m.fetch_tracks.main(self.artistID)
+            self.tracks = m.fetch_tracks.main(self.albums)
         except e.APIError as err:
             print("Error while fetching tracks: " +
                   e.code_to_str_dict[err.code])
         else:
-            self.tracks = m.fetch_tracks.main(self.artistID)
-            print("Tracks fetched successfully.")
+            print(
+                f"Successfully fetched {len(self.tracks)} tracks of {self.name}.")
+            print("Track IDs: " + str(self.tracks))
 
     def show_artist_info(self):
         m.print_albums.main(self.albums)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import classes as c
 
-ConnorPrice = c.Artist(name="Connor Price", artistID="5zixe6AbgXPqt4c1uSl94L")
-MalikHarris = c.Artist(name="Malik Harris", artistID="4Z8W4fKeB5YxbusRsdQVPb")
+ConnorPrice = c.Artist(name="Connor Price", id="5zixe6AbgXPqt4c1uSl94L")
+MalikHarris = c.Artist(name="Malik Harris", id="4Z8W4fKeB5YxbusRsdQVPb")
 
 ConnorPrice.fetch_albums()
+ConnorPrice.fetch_tracks()
